cf9c1db03b163c93cb63358bb6aa9fb7239ba659
idk <hankhill19580@gmail.com>
2022-10-05 01:30:56 -0400

    woo it's an app

---
+++ go.mod
@@ -0,0 +1,37 @@
+module github.com/eyedeekay/mooz
+
+go 1.19
+
+require (
+	fyne.io/systray v1.10.0
+	github.com/atotto/clipboard v0.1.4
+	github.com/christianhujer/isheadless v0.0.0-20181114123347-cbcc774d58b8
+	github.com/eyedeekay/go-i2pbrowser v0.0.0-20221005023356-06e7227788d3
+	github.com/yuukanoo/rtchat v0.0.0-20200410072209-1f8cccac0104
+)
+
+require (
+	github.com/artdarek/go-unzip v1.0.0 // indirect
+	github.com/cretz/bine v0.2.0 // indirect
+	github.com/eyedeekay/go-fpw v0.0.0-20220925184713-d1d68bd3ce7b // indirect
+	github.com/eyedeekay/i2pkeys v0.0.0-20220804220722-1048b5ce6ba7 // indirect
+	github.com/eyedeekay/onramp v0.0.0-20220829050101-64cb1842d0f0 // indirect
+	github.com/eyedeekay/sam3 v0.33.3 // indirect
+	github.com/go-chi/chi v4.0.3+incompatible // indirect
+	github.com/godbus/dbus/v5 v5.0.4 // indirect
+	github.com/google/go-github v17.0.0+incompatible // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/pion/logging v0.2.2 // indirect
+	github.com/pion/stun v0.3.3 // indirect
+	github.com/pion/transport v0.8.10 // indirect
+	github.com/pion/turn/v2 v2.0.3 // indirect
+	github.com/tevino/abool v1.2.0 // indirect
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
+	golang.org/x/net v0.0.0-20220805013720-a33c5aa5df48 // indirect
+	golang.org/x/sys v0.0.0-20220804214406-8e32c043e418 // indirect
+)
+
+replace github.com/yuukanoo/rtchat => github.com/eyedeekay/rtchat v0.0.0-20221005052743-ffcdf3afdf26
+
+replace github.com/artdarek/go-unzip => github.com/eyedeekay/go-unzip v0.0.0-20220914222511-f2936bba53c2
---
+++ main.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"runtime"
+	"strconv"
+	"time"
+
+	"os"
+	"os/exec"
+	"os/signal"
+	"syscall"
+
+	"fyne.io/systray"
+	"fyne.io/systray/example/icon"
+	"github.com/atotto/clipboard"
+	"github.com/christianhujer/isheadless"
+	goi2pbrowser "github.com/eyedeekay/go-i2pbrowser"
+	server "github.com/yuukanoo/rtchat/cmd"
+)
+
+func launch() bool {
+	present := commandExists("firefox")
+	log.Println("Firefox presence test indicates: ", present)
+	env := os.Getenv("mooz_no_launch_app")
+	log.Println("mooz_no_launch_app value", env)
+	switch env {
+	case "false":
+		return false
+	case "0":
+		return false
+	case "f":
+		return false
+	case "":
+		return true
+	case "true":
+		return true
+	case "1":
+		return true
+	case "t":
+		return true
+	default:
+		if i, err := strconv.Atoi(env); err == nil {
+			if i > 0 {
+				return true
+			}
+			if i <= 0 {
+				return false
+			}
+		}
+		return commandExists("firefox")
+	}
+}
+
+func commandExists(cmd string) bool {
+	_, err := exec.LookPath(cmd)
+	return err == nil
+}
+
+var addr = []string{""}
+var dir = ""
+
+func main() {
+	e := server.Flags{
+		Turn: server.TurnFlags{
+			RealmString:    flag.String("realm", "mooz.i2p", "Realm used by the turn server."),
+			PublicIPString: flag.String("turn-ip", "127.0.0.1", "IP Address that TURN can be contacted on. Should be publicly available."),
+			PortInt:        flag.Int("turn-port", 3478, "Listening port for the TURN/STUN endpoint."),
+		},
+		Web: server.WebFlags{
+			Port: flag.Int("http-port", 5000, "Web server listening port."),
+		},
+		I2p: server.I2pFlags{
+			SamIP:   flag.String("sam-ip", "127.0.0.1", "IP address on which the Simple Anonymous Messaging bridge can be reached"),
+			SamPort: flag.Int("sam-port", 7656, "Port on which the Simple Anonymous Messaging bridge can be reached"),
+		},
+	}
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	d := flag.String("dir", wd, "directory to store application state in")
+	host := flag.String("upstream", "https://2dtts2j37mhny4ldp2pjp7iajvp5db426fk3dqtfcuih2w3eivja.b32.i2p/", "Third-party WebRTC chat host")
+	hosted := flag.Bool("hosted", false, "Use third-party WebRTC chat host")
+	launch := flag.Bool("app", launch(), "Start the application")
+	tray := flag.Bool("tray", !isheadless.IsHeadless(), "Show the application running in the system tray")
+	flag.Parse()
+	dir = *d
+	e.Turn.I2p = e.I2p
+	if !*hosted {
+		go func() {
+			addr[0] = server.Serve(e, *e.Turn.RealmString)
+			log.Println("Server started")
+		}()
+		for {
+			if addr[0] != "" {
+				break
+			}
+			time.Sleep(time.Second * 2)
+		}
+		log.Println(addr)
+	}
+	if *hosted {
+		addr[0] = *host
+	}
+
+	go func() {
+		if *tray {
+			systray.Run(onReady, onExit)
+		}
+	}()
+	if *launch {
+		go goi2pbrowser.BrowseApp(dir, addr...)
+	}
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	<-sigs
+
+	log.Println("Shutting down, goodbye 👋")
+}
+
+func onReady() {
+	systray.SetIcon(icon.Data)
+	systray.SetTitle("mooz")
+	systray.SetTooltip("Video calls over I2P")
+	mBrowse := systray.AddMenuItem("Launch call", "Open a window where a call can happen")
+	mCopy := systray.AddMenuItem("Copy URL", "Copy the URL of your Voice Chat")
+	mQuit := systray.AddMenuItem("Quit", "Quit the whole app")
+	go func() {
+		<-mQuit.ClickedCh
+		fmt.Println("Requesting quit")
+		systray.Quit()
+		fmt.Println("Finished quitting")
+	}()
+	go func() {
+		for {
+			select {
+			case <-mBrowse.ClickedCh:
+				go goi2pbrowser.BrowseApp(dir, addr...)
+			case <-mCopy.ClickedCh:
+				clipboard.WriteAll(addr[0])
+			}
+			time.Sleep(time.Second)
+		}
+	}()
+	// Sets the icon of a menu item.
+	mQuit.SetIcon(icon.Data)
+}
+
+func onExit() {
+	// clean up here
+	if runtime.GOOS == "windows" {
+		syscall.Kill(syscall.Getpid(), syscall.SIGKILL)
+	} else {
+		syscall.Kill(syscall.Getpid(), syscall.SIGINT)
+	}
+}
